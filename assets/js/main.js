c1628d60bad5bf0280934645105182c2a1be15f6
zalamanga <rafikmas78@gmail.com>
2024-07-10 16:33:45 +0700

    com

---
+++ assets/js/main.js
@@ -0,0 +1,258 @@
+(function () {
+  ("use strict");
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach((e) => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
+
+  /**
+   * Easy on scroll event listener
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener("scroll", listener);
+  };
+  /**
+   * Navbar links active state on page load
+   */
+  const setActiveNavLink = () => {
+    let navbarlinks = document.querySelectorAll("#navbar .nav-link");
+    let currentPage = window.location.pathname.split("/").pop();
+
+    navbarlinks.forEach((navbarlink) => {
+      if (navbarlink.getAttribute("href") === currentPage) {
+        navbarlink.classList.add("active");
+      } else {
+        navbarlink.classList.remove("active");
+      }
+    });
+  };
+
+  // Call the function when the page loads
+  window.addEventListener("load", setActiveNavLink);
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select("#header");
+    let offset = header.offsetHeight;
+
+    let elementPos = select(el).offsetTop;
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: "smooth",
+    });
+  };
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select("#header");
+  let selectTopbar = select("#topbar");
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add("header-scrolled");
+        if (selectTopbar) {
+          selectTopbar.classList.add("topbar-scrolled");
+        }
+      } else {
+        selectHeader.classList.remove("header-scrolled");
+        if (selectTopbar) {
+          selectTopbar.classList.remove("topbar-scrolled");
+        }
+      }
+    };
+    window.addEventListener("load", headerScrolled);
+    onscroll(document, headerScrolled);
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select(".back-to-top");
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 5000) {
+        backtotop.classList.add("active");
+      } else {
+        backtotop.classList.remove("active");
+      }
+    };
+    window.addEventListener("load", toggleBacktotop);
+    onscroll(document, toggleBacktotop);
+  }
+
+  /**
+   * Back to WA button
+   */
+  let backtowa = select(".back-to-wa");
+  if (backtowa) {
+    const toggleBacktowa = () => {
+      if (window.scrollY > 1000) {
+        backtowa.classList.add("active");
+      } else {
+        backtowa.classList.remove("active");
+      }
+    };
+    window.addEventListener("load", toggleBacktowa);
+    onscroll(document, toggleBacktowa);
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on("click", ".mobile-nav-toggle", function (e) {
+    select("#navbar").classList.toggle("navbar-mobile");
+    this.classList.toggle("bi-list");
+    this.classList.toggle("bi-x");
+  });
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on(
+    "click",
+    ".navbar .dropdown > a",
+    function (e) {
+      if (select("#navbar").classList.contains("navbar-mobile")) {
+        e.preventDefault();
+        this.nextElementSibling.classList.toggle("dropdown-active");
+      }
+    },
+    true
+  );
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on(
+    "click",
+    ".scrollto",
+    function (e) {
+      if (select(this.hash)) {
+        e.preventDefault();
+
+        let navbar = select("#navbar");
+        if (navbar.classList.contains("navbar-mobile")) {
+          navbar.classList.remove("navbar-mobile");
+          let navbarToggle = select(".mobile-nav-toggle");
+          navbarToggle.classList.toggle("bi-list");
+          navbarToggle.classList.toggle("bi-x");
+        }
+        scrollto(this.hash);
+      }
+    },
+    true
+  );
+
+  
+  /**
+   * Hero carousel indicators
+   */
+  let heroCarouselIndicators = document.querySelector(
+    "#hero-carousel-indicators"
+  );
+  let heroCarouselItems = document.querySelectorAll(
+    "#heroCarousel .carousel-item"
+  );
+
+  heroCarouselItems.forEach((item, index) => {
+    let indicator = document.createElement("li");
+    indicator.setAttribute("data-bs-target", "#heroCarousel");
+    indicator.setAttribute("data-bs-slide-to", index.toString());
+
+    if (index === 0) {
+      indicator.classList.add("active");
+    }
+
+    heroCarouselIndicators.appendChild(indicator);
+  });
+
+  /**
+   * Testimonials slider
+   */
+  new Swiper(".testimonials-slider", {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: "auto",
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+  });
+})()
+
+
+
+
+/**
+* Animation on scroll
+*/
+window.addEventListener('load', () => {
+  AOS.init({
+    duration: 1000,
+    easing: 'ease-in-out',
+    once: true,
+    mirror: false
+  })
+});
+
+
+// Featured Products Carousel
+new Swiper('.featured-products-carousel', {
+  speed: 400,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    }
+  }
+});
+
